8be31cdc28ad8c398f3d221000e0d5a563db8bdc
Blue-Onion <rawatadityasingh963@gmail.com>
2026-04-07 22:14:19 +0530

    feat: implement chi router with cors middleware and start server on port 9000

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/go-chi/chi"
+
+	"github.com/go-chi/cors"
+)
+
+const PORT = "9000"
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	server:=http.Server{
+		Handler: router,
+		Addr: PORT,
+	}
+	fmt.Println("Hello")
+	err:=server.ListenAndServe()
+	if err!=nil{
+		log.Fatal(err.Error())
+	}
 }
